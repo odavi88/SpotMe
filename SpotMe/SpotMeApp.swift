215e831613a319abb7e7a60edd3e461a8f72ad2d
Omar Davidson <omardavidson227@gmail.com>
2026-02-12 08:33:37 -0600

    initial commit

---
+++ SpotMe/SpotMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotMeApp.swift
+//  SpotMe
+//
+//  Created by Omar Davidson II on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

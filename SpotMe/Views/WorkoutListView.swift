6a86e3a102ae74cfb97c4337b1d8c4ea18fd74e7
Omar Davidson <omardavidson227@gmail.com>
2026-02-16 08:33:48 -0500

    deleting ContentView; creating WorkoutListView

---
+++ SpotMe/Views/WorkoutListView.swift
@@ -0,0 +1,22 @@
+//
+//  WorkoutListView.swift
+//  SpotMe
+//
+//  Created by Omar Davidson II on 2/16/26.
+//
+
+import SwiftUI
+
+struct WorkoutListView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                
+            }.navigationTitle("My Workouts")
+        }
+    }
+}
+
+#Preview {
+    WorkoutListView()
+}

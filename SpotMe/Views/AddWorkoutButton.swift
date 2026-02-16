d8faa26682b6ffe7a3cec41f97b55e9f2d9d94b6
Omar Davidson <omardavidson227@gmail.com>
2026-02-16 08:43:23 -0500

    creating AddWorkoutButton view with plus label

---
+++ SpotMe/Views/AddWorkoutButton.swift
@@ -0,0 +1,24 @@
+//
+//  AddWorkoutButton.swift
+//  SpotMe
+//
+//  Created by Omar Davidson II on 2/16/26.
+//
+
+import SwiftUI
+
+struct AddWorkoutButton: View {
+    var body: some View {
+        VStack {
+            Button {
+                //
+            } label: {
+                Label("", systemImage: "plus")
+            }
+        }
+    }
+}
+
+#Preview {
+    AddWorkoutButton()
+}

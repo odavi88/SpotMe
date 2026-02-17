03ac6c223161a35029d00878544160ff1d269bd3
Omar Davidson <omardavidson227@gmail.com>
2026-02-17 14:18:35 -0500

    adding WorkoutInputView with input textField for workout title and DatePicker for workout date

---
+++ SpotMe/Views/WorkoutInputView.swift
@@ -0,0 +1,28 @@
+//
+//  WorkoutInputView.swift
+//  SpotMe
+//
+//  Created by Omar Davidson II on 2/17/26.
+//
+
+import SwiftUI
+
+struct WorkoutInputView: View {
+    @State private var workoutTitleText: String = ""
+    @State private var selectedDate: Date = Date()
+    var body: some View {
+        VStack {
+            TextField("Workout Name...", text: $workoutTitleText).textFieldStyle(.roundedBorder)
+            DatePicker("Workout Date", selection: $selectedDate)
+            Button {
+                //
+            } label: {
+                Text("Add Workout")
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    WorkoutInputView()
+}
